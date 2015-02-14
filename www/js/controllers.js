91497d49f10c8fb2543795333a1eb7644dd7bdf5
miwurster <miwurster@googlemail.com>
2015-02-14 13:08:33 +0100

    Converted WAV files to MP3

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,39 +2,39 @@
 
 angular.module('loo.controllers', [])
 
-    .controller('LooController', function ($scope, $ionicPlatform, $settings, $media) {
+  .controller('LooController', function ($scope, $ionicPlatform, $settings, $media) {
 
+    $scope.active = false;
+
+    $scope.play = function () {
+      if (!$scope.active) {
+        $media.play($settings.$find().data.sound.selected);
+      } else {
+        $media.stop();
+      }
+      $scope.active = !$scope.active;
+    };
+
+    $ionicPlatform.ready(function () {
+      document.addEventListener("pause", function () {
+        $media.stop();
         $scope.active = false;
-
-        $scope.play = function () {
-            if (!$scope.active) {
-                $media.play($settings.$find().data.sound.selected);
-            } else {
-                $media.stop();
-            }
-            $scope.active = !$scope.active;
-        };
-
-        $ionicPlatform.ready(function () {
-            document.addEventListener("pause", function () {
-                $media.stop();
-                $scope.active = false;
-                $scope.$apply();
-            }, false);
-        });
-    })
-
-    .controller('SettingsController', function ($scope, $settings) {
-
-        var model = $settings.$find();
-
-        function init() {
-            $scope.settings = model.data;
-        }
-
-        $scope.onChangeSound = function (sound) {
-            model.setData($scope.settings);
-        };
-
-        init();
+        $scope.$apply();
+      }, false);
     });
+  })
+
+  .controller('SettingsController', function ($scope, $settings) {
+
+    var model = $settings.$find();
+
+    function init() {
+      $scope.settings = model.data;
+    }
+
+    $scope.onChangeSound = function (sound) {
+      model.setData($scope.settings);
+    };
+
+    init();
+  });
--- www/js/services.js
+++ www/js/services.js
@@ -4,93 +4,94 @@ angular.module('loo.services', []);
 
 angular.module('loo.services')
 
-    .factory('$media', function ($cordovaMedia) {
+  .factory('$media', function ($cordovaMedia) {
 
-        var media = {
-            play: function () {
-            },
-            stop: function () {
-            }
-        };
+    var media = {
+      play: function () {
+      },
+      stop: function () {
+      }
+    };
 
-        return {
-            play: function (sound) {
-                if (window.Media) {
-                    media = $cordovaMedia.newMedia('/android_asset/www/media/' + sound + '.wav');
-                }
-                media.play();
-            },
-            stop: function () {
-                media.stop();
-            }
-        };
-    }
+    return {
+      play: function (sound) {
+        if (window.Media) {
+          media = $cordovaMedia.newMedia('/android_asset/www/media/' + sound + '.wav');
+        }
+        media.play();
+        return media;
+      },
+      stop: function () {
+        media.stop();
+      }
+    };
+  }
 );
 
 (function () {
 
-    function Settings(data) {
-        if (data && angular.isFunction(data.then)) {
-            this.$unwrap(data);
-            return;
-        }
-        angular.extend(this, {data: data});
+  function Settings(data) {
+    if (data && angular.isFunction(data.then)) {
+      this.$unwrap(data);
+      return;
     }
+    angular.extend(this, {data: data});
+  }
 
-    Settings.$factory = function ($timeout, DSCacheFactory) {
-        angular.extend(Settings, {
-            $timeout: $timeout
-        });
+  Settings.$factory = function ($timeout, DSCacheFactory) {
+    angular.extend(Settings, {
+      $timeout: $timeout
+    });
 
-        Settings.$cache = DSCacheFactory('cache', {
-            storageMode: 'localStorage'
-        });
+    Settings.$cache = DSCacheFactory('cache', {
+      storageMode: 'localStorage'
+    });
 
-        return Settings;
+    return Settings;
+  };
+
+  angular.module('loo.services').factory('$settings', Settings.$factory);
+
+  Settings.prototype.$unwrap = function (futureData) {
+    var self = this;
+    this.$futureData = futureData;
+    this.$futureData.then(function (data) {
+      Settings.$timeout(function () {
+        angular.extend(self, {data: data});
+      });
+    });
+  };
+
+  Settings.prototype.setData = function (data) {
+    this.data = data;
+    Settings.$save(data);
+  };
+
+  Settings.$find = function () {
+    var DEFAULTS = {
+      sound: {
+        selected: 'water-tap',
+        items: [
+          {
+            id: 0,
+            name: 'water-tap',
+            display_name: 'A running water-tap',
+            file: '/android_asset/www/media/water-tap.mp3'
+          },
+          {
+            id: 1,
+            name: 'shower',
+            display_name: 'A running shower',
+            file: '/android_asset/www/media/shower.mp3'
+          }
+        ]
+      }
     };
+    return new Settings(Settings.$cache.get('settings') || DEFAULTS);
+  };
 
-    angular.module('loo.services').factory('$settings', Settings.$factory);
-
-    Settings.prototype.$unwrap = function (futureData) {
-        var self = this;
-        this.$futureData = futureData;
-        this.$futureData.then(function (data) {
-            Settings.$timeout(function () {
-                angular.extend(self, {data: data});
-            });
-        });
-    };
-
-    Settings.prototype.setData = function (data) {
-        this.data = data;
-        Settings.$save(data);
-    };
-
-    Settings.$find = function () {
-        var DEFAULTS = {
-            sound: {
-                selected: 'water-tap',
-                items: [
-                    {
-                        id: 0,
-                        name: 'water-tap',
-                        display_name: 'A running water-tap',
-                        file: '/android_asset/www/media/water-tap.wav'
-                    },
-                    {
-                        id: 1,
-                        name: 'shower',
-                        display_name: 'A running shower',
-                        file: '/android_asset/www/media/shower.wav'
-                    }
-                ]
-            }
-        };
-        return new Settings(Settings.$cache.get('settings') || DEFAULTS);
-    };
-
-    Settings.$save = function (data) {
-        Settings.$cache.put('settings', data);
-    }
+  Settings.$save = function (data) {
+    Settings.$cache.put('settings', data);
+  }
 
 })();
