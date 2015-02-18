b6b9518622e3f177c7a4cbca707b6c0ee52460f0
miwurster <miwurster@googlemail.com>
2015-02-18 12:45:09 +0100

    todo

--- gulpfile.js
+++ gulpfile.js
@@ -1,103 +1,104 @@
+'use strict';
+
 var gulp = require('gulp');
-var gutil = require('gulp-util');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var usemin = require('gulp-usemin');
-var uglify = require('gulp-uglify');
-var minifyHtml = require('gulp-minify-html');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var del = require('del');
-var bower = require('bower');
-var sh = require('shelljs');
-var jshint = require('gulp-jshint');
-var templateCache = require('gulp-angular-templatecache');
-var ngAnnotate = require('gulp-ng-annotate');
+var $ = require('gulp-load-plugins')();
 
 var paths = {
-  sass: ['./app/scss/**/*.scss'],
+  src: 'app',
+  dist: 'www',
+  tmp: '.tmp'
+};
+
+var files = {
+  sass: {
+    main: './app/scss/app.scss'
+    watch: ['./app/scss/**/*.scss']
+  },
   scripts: ['./app/scripts/**/*.js'],
   templates: ['./app/templates/**/*.tpl.html'],
   static: ['./app/images/**/*', './app/media/**/*'],
   fonts: ['./bower_components/ionic/fonts/*']
 };
 
-gulp.task('default', ['jshint', 'sass', 'usemin', 'copy:templates', 'copy:static', 'copy:fonts']);
-gulp.task('build', ['default']);
-gulp.task('release', ['uglify:www']);
-
-gulp.task('clean', function () {
-  return del(['./www', './.tmp']);
+gulp.task('default', ['clean'], function () {
+  gulp.start('build');
 });
 
+gulp.task('clean', function (done) {
+  $.del([paths.dist + '/', paths.tmp + '/'], done);
+});
+
+gulp.task('build', ['jshint', 'sass', 'usemin', 'copy:templates', 'copy:static', 'copy:fonts']);
+gulp.task('release', ['uglify:dist']);
+
 gulp.task('jshint', function () {
-  return gulp.src(paths.scripts)
-    .pipe(jshint())
+  return gulp.src(files.scripts)
+    .pipe($.jshint())
     .pipe(jshint.reporter('jshint-stylish'))
     .pipe(jshint.reporter('fail'));
 });
 
 gulp.task('sass', function () {
-  return gulp.src('./app/scss/app.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./.tmp/css/'));
+  return gulp.src(files.sass.main)
+    .pipe($.sass())
+    .pipe(gulp.dest(paths.tmp + '/css/'));
 });
 
-gulp.task('usemin', ['sass'], function () {
-  return gulp.src('./app/index.html')
-    .pipe(usemin({
-      css: [minifyCss({
-        keepSpecialComments: 0
-      })],
-      html: [minifyHtml({empty: true})],
-      js: [ngAnnotate({
-        add: true,
-        remove: true,
-        single_quotes: true
-      })]
-    }))
-    .pipe(gulp.dest('./www/'));
-});
-
-gulp.task('uglify:www', function () {
-  return gulp.src('./www/scripts/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('./www/scripts'));
-});
-
-gulp.task('copy:templates', function () {
-  return gulp.src(paths.templates)
-    .pipe(minifyHtml({
-      quotes: true
-    }))
-    .pipe(templateCache({
-      'filename': 'template-cache.js',
-      'root': 'templates',
-      'module': 'loo'
-    }))
-    .pipe(gulp.dest('./www'));
-});
-
-gulp.task('copy:static', function () {
-  return gulp.src(paths.static, {base: 'app'})
-    .pipe(gulp.dest('./www'));
-});
-
-gulp.task('copy:fonts', function () {
-  return gulp.src(paths.fonts, {base: 'bower_components'})
-    .pipe(gulp.dest('./www/bower_components'));
-});
-
-gulp.task('copy:sass', ['sass'], function () {
-  return gulp.src('.tmp/css/app.css')
-    .pipe(gulp.dest('./www/css'));
-});
-
-gulp.task('watch', function () {
-  gulp.watch(paths.sass, ['sass', 'copy:sass']);
-  gulp.watch(paths.scripts, ['usemin']);
-  gulp.watch(paths.templates, ['copy:templates']);
-});
+//gulp.task('usemin', ['sass'], function () {
+//  return gulp.src('./app/index.html')
+//    .pipe(usemin({
+//      css: [minifyCss({
+//        keepSpecialComments: 0
+//      })],
+//      html: [minifyHtml({empty: true})],
+//      js: [ngAnnotate({
+//        add: true,
+//        remove: true,
+//        single_quotes: true
+//      })]
+//    }))
+//    .pipe(gulp.dest('./www/'));
+//});
+//
+//gulp.task('uglify:www', function () {
+//  return gulp.src('./www/scripts/*.js')
+//    .pipe(uglify())
+//    .pipe(gulp.dest('./www/scripts'));
+//});
+//
+//gulp.task('copy:templates', function () {
+//  return gulp.src(paths.templates)
+//    .pipe(minifyHtml({
+//      quotes: true
+//    }))
+//    .pipe(templateCache({
+//      'filename': 'template-cache.js',
+//      'root': 'templates',
+//      'module': 'loo'
+//    }))
+//    .pipe(gulp.dest('./www'));
+//});
+//
+//gulp.task('copy:static', function () {
+//  return gulp.src(paths.static, {base: 'app'})
+//    .pipe(gulp.dest('./www'));
+//});
+//
+//gulp.task('copy:fonts', function () {
+//  return gulp.src(paths.fonts, {base: 'bower_components'})
+//    .pipe(gulp.dest('./www/bower_components'));
+//});
+//
+//gulp.task('copy:sass', ['sass'], function () {
+//  return gulp.src('.tmp/css/app.css')
+//    .pipe(gulp.dest('./www/css'));
+//});
+//
+//gulp.task('watch', function () {
+//  gulp.watch(paths.sass, ['sass', 'copy:sass']);
+//  gulp.watch(paths.scripts, ['usemin']);
+//  gulp.watch(paths.templates, ['copy:templates']);
+//});
 
 gulp.task('install', ['git-check'], function () {
   return bower.commands.install()
